ffd01e42f5eceb67369eb24be062b0bb00504f1a
PierreWTH <wietrichpierre@gmail.com>
2023-02-13 22:56:22 +0100

    exercice 15 fini

--- Exo15.php
+++ Exo15.php
@@ -13,4 +13,83 @@ Alice DUCHEMIN a … ans
 
 <?php
 
-// Pas a faire tout de suite
+class Personne{
+
+    // Déclaration propriétés
+
+    private string $_nom;
+    private string $_prenom;
+    private string $_datenaissance;
+
+    // Construct
+
+    public function __construct($nom, $prenom, $datenaissance)
+    {
+        $this->_nom = $nom;
+        $this->_prenom = $prenom;
+        $this->_datenaissance = $datenaissance;
+    }
+
+    // Méthodes
+
+        // Calcul de l'âge
+
+    public function calcAge()
+    {
+        $ajd = date("Y-m-d");
+        $diff = date_diff(date_create($this->_datenaissance), date_create($ajd));
+        $age = $diff->format('%y');
+        return $age;
+    }
+
+        // Affichage des infos
+
+    public function afficherInfos(){
+        $age = $this->calcAge();
+        echo $this->_nom. " " .$this->_prenom. " à " .$age. " ans.<br>";
+    }
+
+    // Getters and Setters
+
+        //Nom
+
+    public function getNom(){
+        return $this->_nom;
+    }
+
+    public function setNom(string $nom){
+        return $this->_nom = $nom;
+    }
+        // Prenom
+
+    public function getPrenom(){
+        return $this->_prenom;
+    }
+
+    public function setPrenom(string $prenom){
+        return $this->_prenom = $prenom;
+    }
+
+        // Date naissance
+
+    public function getDatenaissance(){
+        return $this->_datenaissance;
+    }
+
+    public function setDatenaissance(string $datenaissance){
+        return $this->_datenaissance = $datenaissance;
+    }
+
+
+    // Créations instance
+
+}
+
+$p1 = new Personne("DUPONT", "Michel", "1985-01-17");
+$p2 = new Personne("POINTAK", "Alice", "1928-09-24");
+
+$p1->afficherInfos();
+$p2->afficherInfos();
+
+
+?>
